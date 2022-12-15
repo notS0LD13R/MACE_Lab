8757ccfc005a52d7bc100fe08119a9d82d95a4aa
notS0LD13R <joelsebin@gmail.com>
2022-12-15 19:55:53 +0530

    exp7:Added new Class FPTree and some of its methods

--- DA/exp7.py
+++ DA/exp7.py
@@ -8,20 +8,49 @@ class Node:
     
     def __str__(self) -> str:
         childstr=" ".join(map(str,self.children.values()))
-        return (self.level * "       " )+("{"+str(self.value)+","+str(self.freq)+"}")+"\n"+childstr
+        return (self.level * "       " )+\
+            ("{"+str(self.value)+","+str(self.freq)+"}")+\
+            "\n"+childstr
     
     def assign(self,item_set:list):
         if not item_set:
             self.freq+=1
             return
         
-        #print(self.value,"=>",self.children.keys(),"||",item_set)
         self.freq+=1
         if item_set[0] not in self.children:
-            self.children[item_set[0]]= Node(value=item_set[0],parent=self,children={},level=self.level+1)        
+            self.children[item_set[0]]= \
+            Node(value=item_set[0],parent=self,children={},level=self.level+1)        
         self.children[item_set[0]].assign(item_set[1:])
 
+
+class FPTree:
+    def __init__(self,root :Node,items :list):
+        self.root=root
+        self.items=items
+        self.pattbase={}
+        for item in items:
+            self.pattbase[item]=\
+                self.pattern_base_gen(self.root,item)
+            print(item,':-',self.pattbase[item])
         
+        
+
+    def pattern_base_gen(self,root :Node,item :str) ->list:
+        if root.value==item:
+            patterns={'items':[],'freq':root.freq}
+            root=root.parent
+            while root.value!='null':
+                patterns['items'].append(root.value)
+                root=root.parent
+            
+            return [patterns] if patterns['items'] else []
+        
+        returnlist=[]
+        for i in root.children.values():
+            returnlist.extend(self.pattern_base_gen(i,item))
+        
+        return returnlist
 
 
 
@@ -63,3 +92,5 @@ print(item_freq)
 print(transactions)
 
 print(root)
+
+FPTree(root,item_freq.keys())
