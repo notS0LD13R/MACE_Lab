5fd22188abf991c0a7f1266dc4bcbb7589091668
notS0LD13R <joelsebin@gmail.com>
2022-12-15 22:56:04 +0530

    Added DecTree class and started the definition of get_attr method

--- DA/exp8.py
+++ DA/exp8.py
@@ -1,5 +1,25 @@
 #Decision Tree
 import pandas as pd
 
+class DecTree:
+    def __init__(self,data):
+        self.attr=self.get_attr(data)
+        self.columns=data.columns
+        self.children={}
+    def get_attr(self,data):
+        info_gain={}
+        for i in self.columns:
+            if data.dtype[i] in ['int64','float64']:
+                info_gain[i]=self.numerical_gain(data,i)
+            else:
+                info_gain[i]=self.nominal_gain(data,i)
+    def nominal_gain(self,data,attr):
+        pass
+    def numerical_gain(self,data,attr):
+        pass
+
+
 data=pd.read_csv("exp8.csv")
-print(data)
+print(data.columns)
+
+DecTree(data)
